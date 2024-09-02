9dc4bb0ef0b8eac5b2379e3f31fec38e5682a7ba
Neobozrim <yavor.b.popov@gmail.com>
2024-09-03 00:23:36 +0300

    improved header animation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 let lastScrollTop = 0;
 const header = document.getElementById('header');
+const delta = 10; // Adjust to set how much scroll is needed before hiding
 
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop) {
-        // Scrolling down
+    if (Math.abs(lastScrollTop - scrollTop) <= delta) {
+        return; // Do nothing if not scrolled more than delta
+    }
+
+    if (scrollTop > lastScrollTop && scrollTop > header.offsetHeight) {
+        // Scrolling down and past the header height
         header.style.top = '-100px'; // Adjust based on your header's height
     } else {
         // Scrolling up
